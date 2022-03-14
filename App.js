15a3f5109ee71bc26011d738781d0c54e492d68d
sflivingwagecoalition <87581040+sflivingwagecoalition@users.noreply.github.com>
2022-03-14 16:28:30 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,107 @@
+import 'react-native-gesture-handler';
+import React  from 'react';
+import { Button, View, Text , Image, StyleSheet,TouchableWithoutFeedback,TouchableOpacity, Dimensions,SafeAreaView  } from 'react-native';
+//import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator} from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import BottomTabStack from './App/components/stacks/BottomTabStack'
+import MyStack from './App/components/stacks/MyStack';
+import DetailComponent from './App/components/screens/DonateScreen/DetailComponent';
+import ModalScreen from './App/components/screens/DonateScreen/ModalScreen';
+//import BottemTabNav from './app/components/stacks/BottemTabNav';
+//import NavigationStack from './App/components/stacks/NavigationStack';
+import HomeNavigator from './App/components/screens/HomeScreen/HomeStack';
+
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { bannerStyle } from './App/components/style/styleSheet';
+import DonateMoney from './App/components/screens/DonateScreen/DonateMoney';
+
+// // testing fonts
+// import {
+//   useFonts,
+//   Roboto_100Thin,
+//   Roboto_100Thin_Italic,
+//   Roboto_300Light,
+//   Roboto_300Light_Italic,
+//   Roboto_400Regular,
+//   Roboto_400Regular_Italic,
+//   Roboto_500Medium,
+//   Roboto_500Medium_Italic,
+//   Roboto_700Bold,
+//   Roboto_700Bold_Italic,
+//   Roboto_900Black,
+//   Roboto_900Black_Italic,
+// } from '@expo-google-fonts/roboto';
+// // testing fonts
+
+
+//  import * as Font from 'expo-font';
+//  import { AppLoading } from 'expo';
+
+const Stack = createStackNavigator();
+
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//   'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+//   'roboto-italic': require('./assets/fonts/Roboto-Italic.ttf'),
+//   'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
+//   });
+//   };
+
+export default class App extends React.Component {
+
+  render() {
+      return (
+        <SafeAreaProvider>
+          <NavigationContainer >
+            <Stack.Navigator 
+              initialRouteName="Home"
+              screenOptions={{
+                headerStyle: { backgroundColor: '#d31623' },
+                headerTintColor: '#fff',
+                headerTitleStyle: { fontWeight: 'bold' },}}
+                // tabBar={props => <BottemTabNav {...props} />}
+            >
+              
+              <Stack.Screen name="TabStack" component={BottomTabStack} 
+                options={{ title: 'San Francisco Living Wage Coalition' }}
+                
+                 // options={{ headerTitle: props => <LogoTitle {...props} /> }}
+              />
+            
+              <Stack.Screen name="Details" component={ DetailComponent} />
+              <Stack.Screen name="Preview" component={ModalScreen} />
+              <Stack.Screen name="donate" component={DonateMoney} />
+             
+
+            </Stack.Navigator>
+          
+          </NavigationContainer>
+          </SafeAreaProvider>   
+      )
+  }
+}
+
+function LogoTitle() {
+  return (
+    <View style={{ flexDirection: "row", width: 300, height:65}}>
+    <Image
+      //style={{ width: Dimensions.width, height: 50 }}
+      style={
+        bannerStyle.logoHeaderImageStyle
+      }
+      source={require('./assets/sflwc_logo_finaltemp.png')}
+    />
+ 
+ <MaterialCommunityIcons
+                name="pencil-ruler"
+                color={"white"}
+                 size= {38}
+              />
+ 
+    </View>
+  );
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+## 41.0.5 (April 4/25/2021) 
+this repostity is a continue of the repository of https://github.com/jmejiamu/sflivingwages-mobil-app
+
+### Expo and React-Navigation Update.
+
+* Download new version of Expo, React and Rect-Navigation, 
+* Update App.js by wrap the application with NavigationContainer 
+* Implement new BottemTabStack.js, AuctionTabStack for new bottem and top tab 
+* Replace old icons with new icons with better representation 
+
+## 41.0.6 (June 6/12/2021)
+
+* New Event Page Layout - Eventlist  (Kunyan)
+* New Campaign Views Style ( Kunyan)
+* New Clock and Close Date display for Auction views
+* Art, Photo item will remains as auction item while Books, CDs, DVD sections provide external link to our shop on the website.
+
+## 41.0.7 (June 6/26/2021)
+
+* New Campaign list view with new pictures. (by Kunyan)
+* New Campaign detail views with new picture. ( by Kunyan)
+* Replace Auction icon with donate icon from FontAwesomeIcon
