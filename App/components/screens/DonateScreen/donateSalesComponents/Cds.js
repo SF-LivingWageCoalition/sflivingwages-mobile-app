399e24af3d3d287c43030341a0c9045a40c9d079
redmejia <reynaldoemejia@gmail.com>
2023-04-27 15:53:22 -0700

    action loader

--- App/components/screens/DonateScreen/donateSalesComponents/Cds.js
+++ App/components/screens/DonateScreen/donateSalesComponents/Cds.js
@@ -1,72 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import {
-  Text,
   FlatList,
- 
+  ActivityIndicator
 } from 'react-native';
 
 import Card from '../Card/Card';
 
-const Cds = ({ cds }) => {
-  const calculateTimeLeft = (value) => {
-    // Set bid end day here
-    let year = new Date().getFullYear();
-    let endDate = value // books[0].closeDate;
-    //Date format: 2021-06-01T12:00:00.000Z
-    let difference = +new Date(endDate) - +new Date();
+const Cds = ({ cds, isLoading }) => {
 
 
-    let timeLeft = {};
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60)
-      };
-    }
-    return timeLeft;
-  }
-
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  });
-  const timerComponents = [];
-
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
-
-    timerComponents.push(
-      <Text>
-        {timeLeft[interval]} {interval}{" "}
-      </Text>
-    );
-  });
-  
   return (
-    <FlatList
-      horizontal={false}
-      data={cds}
-      renderItem={({ item }) => <Card
-        key={item.id}
-        name={item.name}
-        description={item.description}
-        price={item.prices.price}
-        link={item.permalink}
-        image={item.images[0].thumbnail}
-        previwImage={item.images[0].src}
-      />
+    <>
+      {
+        isLoading ? <ActivityIndicator size="large" color="red" /> :
+          <FlatList
+            horizontal={false}
+            data={cds}
+            renderItem={({ item }) => <Card
+              key={item.id}
+              name={item.name}
+              description={item.description}
+              price={item.prices.price}
+              link={item.permalink}
+              image={item.images[0].thumbnail}
+              previwImage={item.images[0].src}
+            />
+            }
+          />
       }
-    />
+    </>
   );
 }
 
