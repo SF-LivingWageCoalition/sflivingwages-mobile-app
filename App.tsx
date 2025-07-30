82f318f95448d9aeda19dbd81e898ea2519e7e9a
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-07-29 23:48:30 -0700

    refactor(project-structure): Remove unneeded dependencies

--- App.tsx
+++ App.tsx
@@ -3,14 +3,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NativeBaseProvider } from "native-base";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import BottomTabStack from "./App/navigation/BottomTabStack";
 import ModalScreen from "./App/screens/DonateScreen/ModalScreen";
+import Events from "./App/screens/HomeScreen/components/EventListScreen";
+import WhoWeAre from "./App/screens/WhoWeAreScreen/WhoWeAreScreen";
 import { colors } from "./App/theme";
 import { fontWeight } from "./App/theme/fontStyles";
-import Events from "./App/screens/HomeScreen/components/EventListScreen";
-import BottomTabStack from "./App/navigation/BottomTabStack";
 import { RootStackParamList } from "./App/types";
-import WhoWeAre from "./App/screens/WhoWeAreScreen/WhoWeAreScreen";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -18,34 +17,34 @@ const App: React.FC = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-          <NativeBaseProvider>
-            <Stack.Navigator
-              screenOptions={{
-                headerStyle: { backgroundColor: colors.light.primary },
-                headerTintColor: colors.light.textOnPrimary,
-                headerTitleStyle: { fontWeight: fontWeight.bold },
-              }}
-            >
-              <Stack.Screen
-                name="TabStack"
-                component={BottomTabStack}
-                options={{ title: "San Francisco Living Wage Coalition" }}
-              />
-              <Stack.Screen
-                name="Preview"
-                component={ModalScreen}
-                initialParams={{ image: "" }}
-              />
-              <Stack.Screen name="Event" component={Events} />
-              <Stack.Screen
-                name="WhoWeAre"
-                options={{ title: "Who We Are" }}
-                component={WhoWeAre}
-              />
-            </Stack.Navigator>
-          </NativeBaseProvider>
-        </NavigationContainer>
-      </SafeAreaProvider>
+        <NativeBaseProvider>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: colors.light.primary },
+              headerTintColor: colors.light.textOnPrimary,
+              headerTitleStyle: { fontWeight: fontWeight.bold },
+            }}
+          >
+            <Stack.Screen
+              name="TabStack"
+              component={BottomTabStack}
+              options={{ title: "San Francisco Living Wage Coalition" }}
+            />
+            <Stack.Screen
+              name="Preview"
+              component={ModalScreen}
+              initialParams={{ image: "" }}
+            />
+            <Stack.Screen name="Event" component={Events} />
+            <Stack.Screen
+              name="WhoWeAre"
+              options={{ title: "Who We Are" }}
+              component={WhoWeAre}
+            />
+          </Stack.Navigator>
+        </NativeBaseProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
