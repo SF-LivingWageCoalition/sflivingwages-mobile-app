24395de076d6a55d499e30b1c6e15fc8c3b16d53
jmejiamu <jmejiamu@gmail.com>
2025-07-31 12:03:51 -0700

    feat: setup redux-toolkit

--- App.tsx
+++ App.tsx
@@ -10,40 +10,47 @@ import WhoWeAre from "./App/screens/WhoWeAreScreen/WhoWeAreScreen";
 import { colors } from "./App/theme";
 import { fontWeight } from "./App/theme/fontStyles";
 import { RootStackParamList } from "./App/types";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store/store";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <NativeBaseProvider>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: { backgroundColor: colors.light.primary },
-              headerTintColor: colors.light.textOnPrimary,
-              headerTitleStyle: { fontWeight: fontWeight.bold },
-            }}
-          >
-            <Stack.Screen
-              name="TabStack"
-              component={BottomTabStack}
-              options={{ title: "San Francisco Living Wage Coalition" }}
-            />
-            <Stack.Screen
-              name="Preview"
-              component={ModalScreen}
-              initialParams={{ image: "" }}
-            />
-            <Stack.Screen name="Event" component={Events} />
-            <Stack.Screen
-              name="WhoWeAre"
-              options={{ title: "Who We Are" }}
-              component={WhoWeAre}
-            />
-          </Stack.Navigator>
-        </NativeBaseProvider>
-      </NavigationContainer>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer>
+            <NativeBaseProvider>
+              <Stack.Navigator
+                screenOptions={{
+                  headerStyle: { backgroundColor: colors.light.primary },
+                  headerTintColor: colors.light.textOnPrimary,
+                  headerTitleStyle: { fontWeight: fontWeight.bold },
+                }}
+              >
+                <Stack.Screen
+                  name="TabStack"
+                  component={BottomTabStack}
+                  options={{ title: "San Francisco Living Wage Coalition" }}
+                />
+                <Stack.Screen
+                  name="Preview"
+                  component={ModalScreen}
+                  initialParams={{ image: "" }}
+                />
+                <Stack.Screen name="Event" component={Events} />
+                <Stack.Screen
+                  name="WhoWeAre"
+                  options={{ title: "Who We Are" }}
+                  component={WhoWeAre}
+                />
+              </Stack.Navigator>
+            </NativeBaseProvider>
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
     </SafeAreaProvider>
   );
 };
