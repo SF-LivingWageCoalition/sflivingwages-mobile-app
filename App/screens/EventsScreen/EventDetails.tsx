d90ac7ed41cf6cc8468d75710708703322deaf07
scottmotion <scott@scottmotion.com>
2025-08-18 11:12:14 -0700

    Create EventDetails.tsx

---
+++ App/screens/EventsScreen/EventDetails.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { colors } from "../../theme";
+import { fontSize, fontWeight } from "../../theme/fontStyles";
+
+const EventDetails: React.FC = () => {
+    return (
+        <ScrollView style={styles.scrollView}>
+            <View style={styles.container}>
+                <Text>Event Details Screen</Text>
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        flex: 1,
+    },
+    scrollView: {
+        flex: 1,
+    },
+})
