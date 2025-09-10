c2ca3c00316ff2e1fd50b0cc7f4159cc821de17a
scottmotion <scott@scottmotion.com>
2025-09-10 10:04:08 -0700

    create AuthNav
    created AuthNav
    created screens for Login, Register, Forgot Password
    added AuthNav to App.tsx
    updated types as needed

---
+++ App/auth/ForgotPasswordScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ForgotPassword: React.FC = () => {
+  return (
+    <View>
+      <Text>Forgot Password Screen</Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;
