cc455d99de23401fe1fc87df71bcdb70e377ed1b
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2026-01-09 20:04:42 -0800

    refactor: Add ImageBackground to CampaignScreen to add bg image to title

--- App/screens/CampaignScreen/CampaignScreen.tsx
+++ App/screens/CampaignScreen/CampaignScreen.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Dimensions, FlatList, StyleSheet, View, Text } from "react-native";
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+} from "react-native";
 import richCampaignsData from "../../assets/richCampaignsData";
 import { colors, textStyles } from "../../theme";
 import CampaignCard from "./components/CampaignCard";
+import goldenGateBridge from "../../assets/images/golden-gate-bridge.png";
 
 const { height } = Dimensions.get("window");
 
@@ -12,12 +20,27 @@ const { height } = Dimensions.get("window");
 const CampaignScreen: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>OUR CAMPAIGNS</Text>
+      {/* Hero section with background image */}
+      <ImageBackground
+        source={goldenGateBridge}
+        style={styles.heroImage}
+        resizeMode="cover"
+      >
+        {/* Text overlay */}
+        <View style={styles.overlay}>
+          <View style={styles.heroContent}>
+            <Text style={styles.heroTitle}>OUR CAMPAIGNS</Text>
+          </View>
+        </View>
+      </ImageBackground>
+
+      {/* Campaign list */}
       <FlatList
         data={richCampaignsData}
         renderItem={({ item }) => (
           <CampaignCard title={item.title} text={item.text} id={item.id} />
         )}
+        contentContainerStyle={styles.listContent}
       />
     </View>
   );
@@ -27,11 +50,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  title: {
-    ...textStyles.h3,
-    color: colors.light.onSurface,
-    textAlign: "center",
-    padding: 16,
+  heroImage: {
+    width: "100%",
+    height: 250,
+    justifyContent: "flex-end",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+  },
+  heroContent: {
+    flex: 1,
+    justifyContent: "flex-end",
+    margin: "auto",
+    paddingBottom: 12,
+  },
+  heroTitle: {
+    ...textStyles.h1,
+    color: colors.light.surface,
+    textTransform: "uppercase",
+  },
+  listContent: {
+    paddingTop: 16,
   },
   spinner: {
     height: height / 2,
